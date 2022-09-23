f0856e3422e2ebb46c2f4f1a3af98ef781470ebb
Endercheif <reet22singh@gmail.com>
2022-09-22 21:26:13 -0700

    fix: sm 0.3.0

--- main.js
+++ main.js
@@ -3,23 +3,23 @@ import { h } from "./highlight.js";
 import { CodeJar } from "https://medv.io/codejar/codejar.js";
 
 const output = document.querySelector("#output");
-
-/** @type {import("./pyodide/pyodide.d.ts").PyodideInterface} */
-const pyodide = await loadPyodide({
-    stdout: (msg) => {
-        output.innerText += `\n${msg}`;
-    },
-    stderr: (msg) => {
-        msg = msg.replaceAll(/\[[0-9;]*m/g, "");
-        output.innerHTML += `\n<span class="error">${msg}</span>`;
-    },
-});
-window.pyodide = pyodide;
 const codeBlock = document.querySelector(".editor");
 
 const jar = CodeJar(codeBlock, (c) => {
-    c.innerHTML = h.codeToHtml(c.textContent, { lang: "samarium" });
+  c.innerHTML = h.codeToHtml(c.textContent || " ", { lang: "samarium" });
 });
+
+/** @type {import("./pyodide/pyodide.d.ts").PyodideInterface} */
+const pyodide = await loadPyodide({
+  stdout: (msg) => {
+    output.innerText += `\n${msg}`;
+  },
+  stderr: (msg) => {
+    msg = msg.replaceAll(/\[[0-9;]*m/g, "");
+    output.innerHTML += `\n<span class="error">${msg}</span>`;
+  },
+});
+window.pyodide = pyodide;
 
 await pyodide.loadPackage("micropip");
 
@@ -29,13 +29,13 @@ await micropip.install('./termcolor-1.1.0-py3-none-any.whl')
 await micropip.install('samarium')`);
 
 await pyodide.runPythonAsync(
-    "import sys\nsys.exit = lambda *a, **b: None\n__name__='samarium'"
+  "import sys\nsys.exit = lambda *a, **b: None\n__name__='samarium'"
 );
 
 export const samarium = pyodide.pyimport("samarium");
-export const ch = samarium.CodeHandler(pyodide.globals);
+export const ch = samarium.Registry(pyodide.globals);
 
 document.querySelector(".run").addEventListener("click", () => {
-    output.innerText = "";
-    samarium.run(codeBlock.textContent, ch);
+  output.innerText = "";
+  samarium.run(jar.toString(), ch);
 });
